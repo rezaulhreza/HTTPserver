af1cec720972429b3e620bb7925ea0b6d8f8e64d
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-02-10 23:26:38 +0000

    logger tested and event called on listner

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 
-//create object / cal Person.
+// //create object / cal Person.
 
-//calling files this way is common js
-//import is on es6/+
+// //calling files this way is common js
+// //import is on es6/+
 
-const Person = require('./person')
+// const Person = require('./person')
 
-//instance of the object
-const Person1 = new Person('Test','20','24')
-
-
-/// assigning the method to the  object instance as the object's value
-//itself are the property of method greeting declared in person.js
-
-Person1.greeting();
+// //instance of the object
+// const Person1 = new Person('Test','20','24')
 
 
+// /// assigning the method to the  object instance as the object's value
+// //itself are the property of method greeting declared in person.js
+
+// Person1.greeting();
+
+
+const Logger = require ('./logger')
+
+//Logger is a class so class instance needs to be created
+
+const logger = new Logger()
+
+logger.on('test', (data) => console.log('called listener', data))
+
+//call log method
+
+logger.log('Test done')
--- logger.js
+++ logger.js
@@ -2,3 +2,26 @@ const EventEmitter = require('events')
 
 // the uuid creates an universal and unique identifier
 const uuid = require('uuid')
+
+// console.log(uuid.v4())
+// console.log(uuid.v4())
+// console.log(uuid.v4())
+// console.log(uuid.v4())
+// console.log(uuid.v4())
+// console.log(uuid.v4())
+// console.log(uuid.v4())
+// console.log(uuid.v4())
+
+//class logger
+
+class Logger extends EventEmitter{
+
+    //method log which takes in a message
+    log(msg){
+
+         //call event
+        this.emit('test',{id:uuid.v4(), msg})
+    }
+}
+
+module.exports = Logger
