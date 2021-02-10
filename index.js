db2510727af4e171f6e3d99321e499a317f762fc
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-02-10 01:06:43 +0000

    class created and method called

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hi")
+
+//create object / cal Person
+const Person = require('./Person')
+
+//instance of the object
+const Person1 = new Person('Test',' OK')
+
+
+/// assigning the methd to the  obejct instance as the object's value
+//itself are the property of method greeting declared in person.js
+
+Person1.greeting();
+
+
--- person.js
+++ person.js
@@ -1,12 +1,26 @@
 // create an object 
 
-const person ={
+// const Person ={
 
-    name:'Peter',
-    city:'London',
-    Age: '30'
+//     name:'Peter',
+//     city:'London',
+//     Age: '30'
+// }
+
+//create class Person
+class Person{
+//class constructor
+constructor(name,city){
+this.name=name;
+this.name=city;
 }
 
+
+//printing the value of the constructor
+greeting(){
+    console.log(`This is a test and the element is ${this.name} and ${this.age}` )
+}
+}
 //to export the file to somewhere else use this
 
-module.exports = person;
+module.exports = Person;
