4cc85cda0688b40b20c45c50ed9a6fe932ee7e7c
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-02-10 22:07:54 +0000

    functions for CRUDinf file and dir

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -11,27 +11,47 @@ const path = require ('path')
 
 // });
 
-//create and write to file straightaway
-// it Asynchronously writes data to a file, replacing the file if it already exists.
+// //create and write to file straightaway
+// // it Asynchronously writes data to a file, replacing the file if it already exists.
 
 
-fs.writeFile(path.join(__dirname,'/demo','hello.txt'),
- 'test done again',
- err =>{
+// fs.writeFile(path.join(__dirname,'/demo','hello.txt'),
+//  'test done again ',
+//  err =>{
+//     if(err) throw err;
+//     console.log('Folder is created and file written...');
+
+// });
+
+
+// //file append
+// //Asynchronously append data to a file, 
+// //creating the file if it does not exist.
+
+// fs.appendFile(path.join(__dirname,'/demo','hello.txt'),
+//  'test done once again',
+//  err =>{
+//     if(err) throw err;
+//     console.log('Folder is created and file written...');
+
+// });
+
+// read file
+fs.readFile(path.join(__dirname,'/demo','hello.txt'),'utf8', (err,data) =>{
     if(err) throw err;
-    console.log('Folder is created and file written...');
+    console.log(data);
 
 });
 
+//rename file
 
-//file append
-//Asynchronously append data to a file, 
-//creating the file if it does not exist.
-
-fs.appendFile(path.join(__dirname,'/demo','hello.txt'),
- 'test done again',
- err =>{
+fs.rename(
+    path.join(__dirname,'/demo','hello.txt'),
+    path.join(__dirname,'/demo', 'hiWorld.txt'),
+    err => 
+    {
     if(err) throw err;
-    console.log('Folder is created and file written...');
+    console.log('Fil renamed');
 
-});
+}
+);
