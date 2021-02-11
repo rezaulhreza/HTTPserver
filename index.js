7c0996ce5dafc8d7c426a9b5dcb2222bb6481ceb
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-02-11 02:13:50 +0000

    read json file done locally

--- index.js
+++ index.js
@@ -11,18 +11,67 @@ const server =http.createServer((req,res)  => {
     //this will hang the browser because it didn't get any response to the browser
     // console.log(req.url)
 
-    //directed to index
-    if(req.url==='/about') {
+//     //directed to index
+//     if(req.url==='/') {
 
-        fs.readFile(path.join(__dirname,'public','about.html'),
-        (err,content) => {
-            res.writeHead(200,{ 'Content-Type' : 'text/html'})
-            res.end(content)
-        }
-        )
+//         fs.readFile(path.join(__dirname,'public','index.html'),
+//         (err,content) => {
+//             res.writeHead(200,{ 'Content-Type' : 'text/html'})
+//             res.end(content)
+//         }
+//         )
 
    
-    }
+//     }
+
+//     if(req.url==='/about') {
+
+//         fs.readFile(path.join(__dirname,'public','about.html'),
+//         (err,content) => {
+//             res.writeHead(200,{ 'Content-Type' : 'text/html'})
+//             res.end(content)
+//         }
+//         )
+
+   
+//     }
+
+//     if(req.url==='/api/users') {
+
+// const users=[
+//     { name:'Scott Turner', city: 'London'},
+//     { name:'David Matt', city: 'Kent'}
+// ]
+
+// res.writeHead(200, { 'Content-Type' : 'application/json' } )
+
+// //converting array of objects into json
+
+//    res.end(JSON.stringify(users))
+//     }
+
+
+//Build file path
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 //now this request needs to be listen on a port
 // putting port inside a variable
